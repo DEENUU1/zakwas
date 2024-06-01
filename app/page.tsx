bd842e52f479c7ba6979ff6b790fc74f607e822d
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-06-01 02:42:45 +0200

    add background for header section and galery image

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,53 @@
 import Image from "next/image";
+import {Image as NextImage} from "@nextui-org/react";
+import React from "react";
 
 export default function Home() {
-  return (
-    <>
-    </>
-  );
+
+	const images = [
+		"/1.jpg",
+		"/2.jpg",
+		"/3.jpg",
+		"/4.jpg",
+		"/5.jpg",
+		"/6.jpg",
+		"/7.jpg",
+		"/8.jpg",
+		"/9.jpg",
+		"/10.jpg",
+		"/11.jpg",
+		"/12.jpg",
+		"/13.jpg",
+		"/14.jpg",
+		"/15.jpg",
+		"/16.jpg",
+		"/17.jpg",
+		"/18.jpg",
+	]
+
+	return (
+		<>
+			<div className="relative w-full min-h-screen">
+				<div className="absolute inset-0 -z-10">
+					<Image src="/outside.jpg" layout="fill" objectFit="cover" alt="bg"/>
+				</div>
+				<div className="h-screen flex flex-col items-center justify-center bg-black bg-opacity-50">
+					<h1 className="text-white text-center text-6xl font-bold">
+						Zakwas
+					</h1>
+					<p className="text-white text-center text-xl">
+						Pizza neapolitańska. Klasyka w nowej odsłonie.
+					</p>
+				</div>
+			</div>
+
+			<div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4 bg-black">
+				{images.map((src, index) => (
+					<div key={index}>
+						<NextImage isBlurred={true} isZoomed={true} className="h-auto max-w-full rounded-lg" src={src} alt={`Image ${index + 1}`}/>
+					</div>
+				))}
+			</div>
+		</>
+	);
 }
---
+++ components/NavigationBar.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+
+export default function NavigationBar() {
+  return (
+    <Navbar
+      classNames={{
+        item: [
+          "flex",
+          "relative",
+          "h-full",
+          "items-center",
+          "data-[active=true]:after:content-['']",
+          "data-[active=true]:after:absolute",
+          "data-[active=true]:after:bottom-0",
+          "data-[active=true]:after:left-0",
+          "data-[active=true]:after:right-0",
+          "data-[active=true]:after:h-[2px]",
+          "data-[active=true]:after:rounded-[2px]",
+          "data-[active=true]:after:bg-primary",
+        ],
+      }}
+    >
+      <NavbarBrand>
+        <p className="font-bold text-inherit">Zakwas</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Contact
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Menu
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+       <NavbarItem>
+          <Link as={Link} color="primary" href="#" variant="flat">
+            Facebook
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
