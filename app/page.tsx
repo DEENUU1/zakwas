3b854882a092587e14659b5a1c62c14c868e4af6
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-06-02 23:50:11 +0200

    feature: add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,26 @@
-import React from "react";
 import Hero from "@/components/Hero";
 import Galery from "@/components/Galery";
 import Menu from "@/components/Menu";
 import GoogleMaps from "@/components/GoogleMapsIFrame";
 import Testimonials from "@/components/Testimonials";
+import type { Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+  title: 'Zakwas',
+  description: 'Pizza neapolitańska. Klasyka w nowej odsłonie.',
+}
+
+export default function Page() {}
 
 export default function Home() {
-
-	const images = [
-		"/1.jpg",
-		"/2.jpg",
-		"/3.jpg",
-		"/4.jpg",
-		"/5.jpg",
-		"/6.jpg",
-		"/7.jpg",
-		"/8.jpg",
-		"/9.jpg",
-		"/10.jpg",
-		"/11.jpg",
-		"/12.jpg",
-		"/13.jpg",
-		"/14.jpg",
-		"/15.jpg",
-		"/16.jpg",
-		"/17.jpg",
-		"/18.jpg",
-	]
 
 	return (
 		<>
 			<Hero/>
 			<Menu/>
 			<Testimonials/>
-			<Galery images={images}/>
+			<Galery/>
 			<GoogleMaps/>
 		</>
 	);
--- components/Galery.tsx
+++ components/Galery.tsx
@@ -2,7 +2,28 @@ import {Image as NextImage} from "@nextui-org/image";
 import React from "react";
 
 
-export default function Galery({images}: {images: string[]}){
+export default function Galery(){
+	const images = [
+		"/1.jpg",
+		"/2.jpg",
+		"/3.jpg",
+		"/4.jpg",
+		"/5.jpg",
+		"/6.jpg",
+		"/7.jpg",
+		"/8.jpg",
+		"/9.jpg",
+		"/10.jpg",
+		"/11.jpg",
+		"/12.jpg",
+		"/13.jpg",
+		"/14.jpg",
+		"/15.jpg",
+		"/16.jpg",
+		"/17.jpg",
+		"/18.jpg",
+	]
+
 	return (
 		<>
 			<h1 className={"text-center text-5xl mt-5 mb-5"}>Galeria smaków</h1>
